e244eb75b3f52d243c1f280b9424d13b742d2406
Sara Susano <sarasusanoruiz02@gmail.com>
2024-03-09 13:19:18 +0100

    Añadi los comentarios que se borraron al exportar.

--- Prototipo/src/main/java/tfg/Prototipo/LeerContenidoPdf.java
+++ Prototipo/src/main/java/tfg/Prototipo/LeerContenidoPdf.java
@@ -10,10 +10,10 @@ public class LeerContenidoPdf {
     public static void main(String[] args) {
         String rutaPDF = "C:/Users/Admin/Desktop/Ejemplo para descargar pdf/Frameworks_Generate_Domain-Specific_Languages_A_Case_Study_in_the_Multimedia_Domain.pdf";
 
-        try (PDDocument document = PDDocument.load(new File(rutaPDF))) {
-            PDFTextStripper textStripper = new PDFTextStripper();
-            String contenido = textStripper.getText(document);
-            System.out.println(contenido);
+        try (PDDocument documento = PDDocument.load(new File(rutaPDF))) {//cargo el documento
+            PDFTextStripper extraer = new PDFTextStripper();//creo instancia de PDFTextStripper
+            String contenido = extraer.getText(documento);//extraigo en la variable contenido el texto del documento
+            System.out.println(contenido);//muestro por pantalla el contenido del documento
         } catch (IOException e) {
             e.printStackTrace();
         }
