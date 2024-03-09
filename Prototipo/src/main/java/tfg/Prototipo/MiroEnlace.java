b50207d1e857c923aebab1c24c5994a063037ec4
Sara Susano <sarasusanoruiz02@gmail.com>
2024-03-09 13:47:05 +0100

    Añado los comentarios que se borraron

--- Prototipo/src/main/java/tfg/Prototipo/MiroEnlace.java
+++ Prototipo/src/main/java/tfg/Prototipo/MiroEnlace.java
@@ -9,31 +9,30 @@ import java.io.IOException;
 public class MiroEnlace {
 
     public static void main(String[] args) {
-        String enlace = "http://www.clam-project.org";
+        String enlace = "http://www.clam-project.org";//este es el enlace del pdf a mirar
 
-        if (verificarExistencia(enlace)) {
-            System.out.println("El enlace existe.");
+        if (verificarExistencia(enlace)) {//llamo a verificarExistencia para mirar si existe el enlace
+            System.out.println("El enlace existe.");//si exsite
         } else {
-            System.out.println("El enlace no existe o no se pudo verificar.");
+            System.out.println("El enlace no existe o no se pudo verificar.");//si no existe o hubo algún problema
         }
     }
-
+//Con esta funcion verifico si existen los enlaces de las referencias de los pdfs
     public static boolean verificarExistencia(String enlace) {
         try {
-            Connection.Response response = Jsoup.connect(enlace)
-                    .followRedirects(true) // Seguir redirecciones
-                    .timeout(1000000) // Establecer tiempo de espera en milisegundos
-                    .method(Connection.Method.HEAD) // Utilizar el método HEAD
-                    .execute();
+            Connection.Response r = Jsoup.connect(enlace)//conecto con el enlace con Jsoup
+                    .followRedirects(true) //Sigo las redirecciones
+                    .timeout(1000000) //Establezco un tiempo de espera en ms
+                    .method(Connection.Method.HEAD) //Utilizo el metodo head
+                    .execute();//lo ejecuto
 
-            // Obtener el código de respuesta
-            int responseCode = response.statusCode();
+            int rCodigo = r.statusCode();//Obtengo el codigo de respuesta que me devuelve el servidor
 
-            // Verificar si la respuesta es 2xx (éxito)
-            return (responseCode >= 200 && responseCode < 300);
+            //Si la respuesta del servidor esta entre 200 y 300 -> 2xx entonces es positiva
+            return (rCodigo >= 200 && rCodigo < 300);
         } catch (IOException e) {
             e.printStackTrace();
-            return false;
+            return false;// si no es negativa
         }
     }
 }
