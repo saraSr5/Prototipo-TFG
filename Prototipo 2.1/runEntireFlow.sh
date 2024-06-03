454ea898a448a90c18de7d37b741bfc7ed2ad81e
saraSr5 <130143896+saraSr5@users.noreply.github.com>
2024-06-03 10:29:46 +0200

    Add files via upload

--- Prototipo 2.1/runEntireFlow.sh
+++ Prototipo 2.1/runEntireFlow.sh
@@ -1,7 +1,13 @@
-java -cp checkurl-0.0.1-SNAPSHOT-jar-with-dependencies.jar es.upm.grise.checkurl.LeerXml $1 > titleList.txt
+java -cp checkurl-0.0.1-SNAPSHOT-jar-with-dependencies.jar es.upm.grise.checkurl.LeerXml $1 > titleList.txt 2> errorTitleList.txt
 
-cat titleList.txt | xargs java -cp checkurl-0.0.1-SNAPSHOT-jar-with-dependencies.jar es.upm.grise.checkurl.ObtenerPdfConSerpApi > urlList.txt
+cat titleList.txt | xargs java -cp checkurl-0.0.1-SNAPSHOT-jar-with-dependencies.jar es.upm.grise.checkurl.ObtenerPdfConSerpApi > urlList.txt 2> errorUrlList.txt
 
-cat urlList.txt | xargs java -cp checkurl-0.0.1-SNAPSHOT-jar-with-dependencies.jar es.upm.grise.checkurl.DescargarPdfCache > fileList.txt
+cat urlList.txt | xargs java -cp checkurl-0.0.1-SNAPSHOT-jar-with-dependencies.jar es.upm.grise.checkurl.DescargarPdfCache > fileList.txt 2> errorFileList.txt
 
-cat fileList.txt | xargs java -cp checkurl-0.0.1-SNAPSHOT-jar-with-dependencies.jar es.upm.grise.checkurl.MostrarYComprobarEnlaces
+cat fileList.txt | xargs java -cp checkurl-0.0.1-SNAPSHOT-jar-with-dependencies.jar es.upm.grise.checkurl.MostrarYComprobarEnlaces > links.txt 2> errorLinks.txt
+
+java -cp checkurl-0.0.1-SNAPSHOT-jar-with-dependencies.jar es.upm.grise.checkurl.ClasificarURL > classification.txt 2> errorClassification.txt
+
+
+
+
